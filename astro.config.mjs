dfa321bfc704b69ae5ad916332dd0a42cf80564d
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-09-16 17:36:40 -0300

    etapa 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    site: "https://moonlit-melba-2a95bd.netlify.app/"
 });
